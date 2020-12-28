5320841a6779364b379d8ec3b00925f0e0738e76
timo_eich <5fhtfqg9z6@privaterelay.appleid.com>
2020-12-28 21:33:57 +0100

    new Test IntersectingWithATransformedGroup

--- RayTracerTests/GroupTests.cs
+++ RayTracerTests/GroupTests.cs
@@ -87,5 +87,25 @@ namespace RayTracerTests
             Assert.IsTrue(intersections[3].Shape.NearlyEquals(sphere_1));
         }
 
+        [Test()]
+        public void IntersectingATransformedGroup()
+        {
+            // Given
+            Sphere sphere = new Sphere();
+            sphere.Transform = Matrix.NewTranslationMatrix(5, 0, 0);
+
+            Group group = new Group();
+            group.Transform = Matrix.NewScalingMatrix(2, 2, 2);
+            group.AddChild(sphere);
+
+            // When
+            Ray ray = new Ray(new Point(10, 0, -10), new Vector(0, 0, 1));
+
+            Intersections intersections = group.GetIntersections(ray);
+
+            // Then
+            Assert.AreEqual(2, intersections.Count);
+        }
+
     }
 }
