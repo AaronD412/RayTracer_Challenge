fd29ee1e398b3f4b33fbbffe0599a6b565082935
timo_eich <5fhtfqg9z6@privaterelay.appleid.com>
2020-12-28 20:11:27 +0100

    changed the test IntersectingARayWithANonEmptyGroup

--- RayTracerTests/GroupTests.cs
+++ RayTracerTests/GroupTests.cs
@@ -51,17 +51,17 @@ namespace RayTracerTests
 
             Ray ray = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1));
 
-            //When
+            // When
             Intersections intersections = group.GetIntersections(ray);
 
-            //Then
+            // Then
             Assert.AreEqual(0, intersections.Count);
         }
 
         [Test()]
         public void IntersectingARayWithAnNonEmptyGroup()
         {
-            // ToDO: implement test intersecting a ray with an empty group.
+            // Given
             Group group = new Group();
 
             Sphere sphere_1 = new Sphere();
@@ -70,6 +70,21 @@ namespace RayTracerTests
 
             sphere_2.Transform = Matrix.NewTranslationMatrix(0, 0, -3);
             sphere_3.Transform = Matrix.NewTranslationMatrix(5, 0, 0);
+
+            group.AddChildren(sphere_1);
+            group.AddChildren(sphere_2);
+            group.AddChildren(sphere_3);
+
+            // When
+            Ray ray = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1));
+            Intersections intersections = group.GetIntersections(ray);
+
+            // Then
+            Assert.AreEqual(4, intersections.Count);
+            Assert.IsTrue(intersections[0].SceneObject.NearlyEquals(sphere_2));
+            Assert.IsTrue(intersections[1].SceneObject.NearlyEquals(sphere_2));
+            Assert.IsTrue(intersections[2].SceneObject.NearlyEquals(sphere_1));
+            Assert.IsTrue(intersections[3].SceneObject.NearlyEquals(sphere_1));
         }
 
     }
